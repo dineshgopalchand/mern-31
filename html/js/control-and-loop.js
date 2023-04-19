2c39ceeb06b3ac9a1d800d62dfdb181493a6fb1b
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-04-19 21:59:20 +0530

    Switchcase discussed

--- html/js/control-and-loop.js
+++ html/js/control-and-loop.js
@@ -24,3 +24,33 @@ if(gender=='M'){
 }else{
     console.log('other')
 }
+
+const currentDate= new Date();
+console.log(currentDate);
+const dayValue=currentDate.getDay();
+// const dayArr=['SUN','MON','TUE','WED','THR','FRI','SAT'];
+// console.log(dayArr[dayValue]);
+
+switch (dayValue) {
+    case 0:
+        console.log('SUN');  
+        break;
+    case 1:
+        console.log('MON');  
+        break;
+    case 2:
+        console.log('TUE');  
+        break;
+    case 3:
+        console.log('WED');  
+        break;
+    case 4:
+        console.log('THR');  
+        break;
+    case 5:
+        console.log('FRI');  
+        break;
+    case 6:
+        console.log('SAT');  
+        break;
+}
