848b6dbab6044a678e78d757059e9bf3185675ec
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-04-19 21:43:07 +0530

    discussed If else condition

---
+++ html/js/control-and-loop.js
@@ -0,0 +1,26 @@
+let numberVal=89889983;
+// let result=numberVal%2==0?'even':'odd';
+// let result='odd';
+
+// if(numberVal%2==0){
+//     result='even';
+// }
+// console.log(result);
+let result='';
+
+if(numberVal%2==0){
+    result='even';
+}else{
+    result='odd';
+}
+console.log(result);
+
+
+const gender='F';
+if(gender=='M'){
+    console.log('Male');
+}else if(gender=='F'){
+    console.log('Female')
+}else{
+    console.log('other')
+}
