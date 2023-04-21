172858ceb56d44e806810a5052d2ad9bb9900207
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-04-21 21:41:04 +0530

    Discussed on pattern

---
+++ html/js/patter2.js
@@ -0,0 +1,19 @@
+// pattern 2
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+totalLineNum=10;
+for(lineCount=1;lineCount<=totalLineNum;lineCount++){
+    // loop for spacing
+    let lineValue='';
+    for(let spaceCount=1;spaceCount<=totalLineNum-lineCount;spaceCount++){
+        lineValue+=' ';
+    }
+    // loop for *(star)
+    for(let starCount=1;starCount<=2*lineCount -1;starCount++){
+        lineValue+='*';
+    }
+    console.log(lineValue);
+}
