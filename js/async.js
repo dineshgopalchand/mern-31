adbf7ec4eb60a2ed85248055af17c1331715e913
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-05-19 21:03:18 +0530

    Created React App

---
+++ js/async.js
@@ -0,0 +1,55 @@
+// console.log(1);
+// setTimeout(function () {
+//   for (let count = 0; count < 10000000000; count++) {
+//     // console.log('count',count);
+//   }
+//   console.log('inside timer function')
+// });
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+
+// setTimeout(function(){
+//     console.log('from setTimeout A')
+// },1001)
+// setTimeout(function(){
+//     console.log('from setTimeout B')
+// },1000)
+// setTimeout(function(){
+//     console.log('from setTimeout C')
+// },10000)
+// setTimeout(function(){
+//     console.log('from setTimeout D')
+// },500)
+// setTimeout(function(){
+//     console.log('from setTimeout E')
+// })
+// console.log('end');
+
+// let countVal=0;
+// let interval=setInterval(function(){
+//     console.log('inside setInterval', new Date());
+//     countVal++;
+//     if(countVal==10){
+//         clearInterval(interval);
+//     }
+//     // console.log(interval);
+// },1000);
+
+
+new Promise((res,rej)=>{
+    // code to  fetch API
+}).then((res)=>{
+    // 5 user id
+    // promise 1
+    // promise 2
+    // promise 3
+    // promise 4
+    // promise 5
+
+    
+},err=>{
+
+})
