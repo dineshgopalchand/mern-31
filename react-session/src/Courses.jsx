5208a94d681a09be473221b61f303da855aa1883
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-07-26 20:28:37 +0530

    Added react-icon module

--- react-session/src/Courses.jsx
+++ react-session/src/Courses.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Styles from "./Courses.module.css";
-
+import {AiFillDelete} from 'react-icons/ai';
+import { Col, Row } from "react-bootstrap";
 
 const Courses = (props) => {
 
   const { course } = props; // destructing props
 
   return (
-    <div className={Styles.courseItem} >
-      <h3>{course?.name}</h3>
-      <p>{course?.details}</p>
-    </div>
+    <Row className={Styles.courseItem} >
+      <Col  ><h3>{course?.name}</h3>
+      <p>{course?.details}</p></Col>
+      <Col md="auto" className="mt-3"> <button className="btn btn-danger"><AiFillDelete /></button></Col>
+     
+      
+      
+    </Row>
   );
 };
 
